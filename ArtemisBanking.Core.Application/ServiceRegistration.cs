10e6c041437fe60a276529636b85e30016cf67a7
home <home@DESKTOP-J7UD23O>
2025-11-06 18:15:18 -0400

    WIP: guardado temporal antes de actualizar

--- ArtemisBanking.Core.Application/ServiceRegistration.cs
+++ ArtemisBanking.Core.Application/ServiceRegistration.cs
@@ -1,7 +1,27 @@
-﻿namespace ArtemisBanking.Core.Application
-{
-    public class ServiceRegistration
-    {
+﻿using ArtemisBanking.Core.Application.Interfaces;
+using ArtemisBanking.Core.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
+namespace ArtemisBanking.Core.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            #region Configuration
+            services.AddAutoMapper(cfg => cfg.AddMaps(Assembly.GetExecutingAssembly()));
+            #endregion
+            #region Services
+            services.AddScoped<IBeneficiaryService, BeneficiaryService>();
+            services.AddScoped<ISavingsAccountService, SavingsAccountService>();
+            services.AddScoped<ITransactionService, TransactionService>();
+            services.AddScoped<ILoanService, LoanService>();
+            services.AddScoped<ICreditCardService, CreditCardService>();
+            services.AddScoped<IInstallmentService, InstallmentService>();
+            services.AddScoped<ICardTransactionService, CardTransactionService>();
+            services.AddScoped<IMerchantService, MerchantService>();
+            #endregion
+        }
     }
 }
