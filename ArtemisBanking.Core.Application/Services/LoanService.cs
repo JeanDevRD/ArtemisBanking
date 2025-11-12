170efa56e75297e3b2742b17469565eb1ef568f2
Zorcis Calderon <zorciscalderon793@gmail.com>
2025-11-11 23:20:57 -0400

    Add Loan

--- ArtemisBanking.Core.Application/Services/LoanService.cs
+++ ArtemisBanking.Core.Application/Services/LoanService.cs
@@ -58,6 +58,31 @@ namespace ArtemisBanking.Core.Application.Services
             }
         }
 
+        public async Task<ResultDto<LoanDto>> AddLoanAsync(LoanDto entityDto)
+        {
+            var result = new ResultDto<LoanDto>();
+
+            var loan = await _loanRepository.GetAllQueryAsync()
+                .Where(x => x.UserId == entityDto.UserId && x.IsActive)
+                .FirstOrDefaultAsync();
+
+            if (loan != null)
+            {
+                result.IsError = true;
+                result.Message = "Este cliente ya tiene un prestamo activo";
+                return result;
+            }
+
+            var newLoan = await base.AddAsync(entityDto);
+
+            result.IsError = false;
+            result.Result = newLoan;
+            return result;
+        }
+
+
+
+
         public async Task<ResultDto<List<LoanListDto>>?> GetAllActiveLoanAsync()
         {
             var result = new ResultDto<List<LoanListDto>>();
@@ -112,23 +137,47 @@ namespace ArtemisBanking.Core.Application.Services
         }
 
 
-        public async Task<ResultDto<LoanListDto>> GetLoanByUserIdentity(string IdentificationNumber) 
-        { 
-            var result = new ResultDto<LoanListDto>();
+        public async Task<ResultDto<List<LoanListDto>>> GetLoansByUserIdentity(string identificationNumber, bool isActive)
+        {
+            var result = new ResultDto<List<LoanListDto>>();
 
-            try 
+            try
             {
-                var client = await _clientApp.GetUserByIdentificationNumber(IdentificationNumber);
+                var client = await _clientApp.GetUserByIdentificationNumber(identificationNumber);
 
                 if (client == null)
                 {
                     result.IsError = true;
                     result.Message = "Usuario no encontrado";
+                    return result;
                 }
 
+                var loansList = await _loanRepository.GetAllQueryAsync().Where(x => x.UserId == client.Id && x.IsActive == isActive).OrderByDescending(x => x.ApprovedAt).ToListAsync();
 
+                if (loansList == null)
+                {
+                    result.IsError = true;
+                    result.Message = "No se encontraron prestamos para este usuario";
+                    return result;
+                }
+
+                var loansDto = loansList.Select(loan => new LoanListDto
+                {
+                    LoanId = loan.Id,
+                    ClientFullName = $"{client.FirstName} {client.LastName}",
+                    CapitalAmount = loan.Amount,
+                    TotalInstallments = loan.Installments?.Count ?? 0,
+                    PaidInstallments = loan.Installments?.Count(i => i.IsPaid) ?? 0,
+                    OutstandingAmount = loan.Installments?.Where(i => !i.IsPaid).Sum(i => i.PaymentAmount) ?? 0,
+                    InterestRate = loan.AnnualInterestRate,
+                    TermInMonths = loan.TermMonths,
+                    PaymentStatus = loan.Installments != null && loan.Installments.Any(i => i.IsLate) ? "En mora" : "Al día"
+                }).ToList();
+
+                result.IsError = false;
+                result.Result = loansDto;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 result.IsError = true;
                 result.Message = ex.Message;
